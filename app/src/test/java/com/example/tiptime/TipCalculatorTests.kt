cab7a65917809d0d1631e19a70a05c1c316be73d
Divyansh Kushwaha <divyanshdxn@gmail.com>
2022-11-11 19:54:23 +0530

    fix: test failure for locale other than US
    The variable expectedTip in calculate_20_percent_tip_no_roundup was hardcoded in a currency format with dollar($), which would fail for different locale.
    This is fixed by formatting the expectedTip according to locale

--- app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
+++ app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -17,6 +17,7 @@ package com.example.tiptime
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.text.NumberFormat
 
 class TipCalculatorTests {
 
@@ -24,7 +25,7 @@ class TipCalculatorTests {
     fun calculate_20_percent_tip_no_roundup() {
         val amount = 10.00
         val tipPercent = 20.00
-        val expectedTip = "$2.00"
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2.00)
         val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
         assertEquals(expectedTip, actualTip)
     }

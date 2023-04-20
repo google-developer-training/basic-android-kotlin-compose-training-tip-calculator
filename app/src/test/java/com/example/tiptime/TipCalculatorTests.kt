86818b7ab4055d70b4538749f819df370fcbf272
Android dev <androidedu@google.com>
2023-04-20 12:11:53 -0700

    Remove dimens.xml
    Update TipCalculatorTests.kt

--- app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
+++ app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -22,7 +22,7 @@ import java.text.NumberFormat
 class TipCalculatorTests {
 
     @Test
-    fun calculate_20_percent_tip_no_roundup() {
+    fun calculateTip_20PercentNoRoundup() {
         val amount = 10.00
         val tipPercent = 20.00
         val expectedTip = NumberFormat.getCurrencyInstance().format(2)

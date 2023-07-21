a67fe0be678500ddbb20c8b2c754fe11c7fdda28
Android Dev <androidedu@google.com>
2023-07-21 16:16:51 -0700

    Groovy to KTS migration - test solution (#121)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,15 +14,21 @@
  * limitations under the License.
  */
 
-buildscript {
-    ext {
-        compose_compiler_version = '1.4.3'
-    }
+@file:Suppress("UnstableApiUsage")
 
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
 }
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id 'com.android.application' version '8.0.0' apply false
-    id 'com.android.library' version '8.0.0' apply false
-    id 'org.jetbrains.kotlin.android' version '1.8.10' apply false
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
+rootProject.name = "Tip Time"
+include(":app")

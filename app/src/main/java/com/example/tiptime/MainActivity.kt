11fa6d3fd11bc4cc41ed27c72eb7c31f27e89f67
Android dev <androidedu@google.com>
2023-08-18 12:26:43 -0700

    Use safeContentPadding

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -24,7 +24,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
@@ -60,7 +60,7 @@ class MainActivity : ComponentActivity() {
 fun TipTimeLayout() {
     Column(
         modifier = Modifier
-            .statusBarsPadding()
+            .safeContentPadding()
             .padding(horizontal = 40.dp)
             .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,

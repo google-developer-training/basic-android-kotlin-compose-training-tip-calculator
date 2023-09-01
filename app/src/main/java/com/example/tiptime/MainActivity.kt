3cb1abc0fb64c33af4180a476483683f610fbfe3
Android dev <androidedu@google.com>
2023-08-31 17:18:54 -0700

    Update MainActivity.kt

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -65,7 +65,6 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
                     TipTimeLayout()
                 }

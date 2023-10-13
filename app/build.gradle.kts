7b57776475cebd4f0f145bc05ffba78449d747b7
Android Dev <testingilt567@gmail.com>
2023-10-13 12:41:48 -0700

    Use new enableEdgeToEdge() api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,13 +68,13 @@ android {
 dependencies {
 
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
     testImplementation("junit:junit:4.13.2")
 

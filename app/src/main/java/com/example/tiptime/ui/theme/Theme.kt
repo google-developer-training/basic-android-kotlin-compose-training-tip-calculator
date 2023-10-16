da622101eb7a178215df457c157df2e0f1c0a68c
Android Dev <testingilt567@gmail.com>
2023-10-16 09:38:09 -0700

    Remove unused imports

--- app/src/main/java/com/example/tiptime/ui/theme/Theme.kt
+++ app/src/main/java/com/example/tiptime/ui/theme/Theme.kt
@@ -15,9 +15,7 @@
  */
 package com.example.tiptime.ui.theme
 
-import android.app.Activity
 import android.os.Build
-import android.view.View
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -25,12 +23,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val LightColorScheme = lightColorScheme(
     primary = md_theme_light_primary,
